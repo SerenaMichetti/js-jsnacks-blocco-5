502d0d007ebc522521ae9b53725b85c76581de61
Serena <sere.michetti@icloud.com>
2025-11-30 21:22:00 +0100

    completato snack 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const student = students.find(student => {
+  return student.name === 'Marco Lanci'
+})
+console.log(student.class)
